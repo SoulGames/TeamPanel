229dccac6deef48f2609a99f2a224ee9da26537c
No Name Pro <noname.pro.nn@gmail.com>
2020-07-01 12:14:43 +0200

    🐛 Some forgot things

--- TeamPanel/Pages/Accounts/Add.cshtml.cs
+++ TeamPanel/Pages/Accounts/Add.cshtml.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Dapper.Contrib.Extensions;
+using Library.Managers;
 using Library.Types;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -15,6 +16,12 @@ namespace TeamPanel.Pages
     {
         private MySqlConnection MySQL;
 
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string Password { get; set; }
+
+
         public AccountsAddModel(MySqlConnection mySQL)
         {
             MySQL = mySQL;
@@ -23,7 +30,22 @@ namespace TeamPanel.Pages
         public void OnGet()
         {
 
+        }
 
+        public IActionResult OnPostAsync()
+        {
+            if (String.IsNullOrEmpty(Password)) { return Page(); }
+            if (String.IsNullOrEmpty(Username)) { return Page(); }
+
+            string PasswordHash = Hash.CalculateBCryptPassword(Password);
+
+            Account Account = new Account();
+            Account.USERNAME = Username;
+            Account.PASSWORD = PasswordHash;
+
+            MySQL.Insert(Account);
+
+            return Page();
         }
     }
 }
