fe6b78d27ddb9456e680583252ca5a63ff209ee9
No Name Pro <noname.pro.nn@gmail.com>
2020-07-11 11:42:50 +0200

    ✨  Betausers

---
+++ TeamPanel/Pages/Betausers/Add.cshtml.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using Library.Managers;
+using Library.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Library.Managers;
+using Library.Types;
+
+namespace TeamPanel.Pages
+{
+    public class BetausersAddModel : PageModel
+    {
+        private MySqlConnection MySQL;
+
+        [BindProperty]
+        public string Username { get; set; }
+        [BindProperty]
+        public string redirect { get; set; }
+
+        public Account LoginUser;
+        public BetausersAddModel(MySqlConnection mySQL)
+        {
+            MySQL = mySQL;
+        }
+
+        public IActionResult OnGet(string redirect)
+        {
+            // Authorization
+            AuthorizationResult authorizationResult;
+            if (!Authorization.CheckAuthorization(HttpContext, MySQL, HttpContext.Response, out authorizationResult)) { return StatusCode(authorizationResult.StatusCode); }
+            LoginUser = authorizationResult.Account;
+
+            return Page();
+        }
+        public IActionResult OnPostAsync()
+        {
+            BetaUser NewBetaUser = new BetaUser();
+            NewBetaUser.USERNAME = Username;
+            MySQL.Insert(NewBetaUser);
+
+            if (String.IsNullOrEmpty(redirect))
+            {
+                return Redirect("/Betausers");
+            }
+            return Redirect(redirect);
+        }
+    }
+}
---
+++ TeamPanel/Pages/Betausers/Delete.cshtml.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapper.Contrib.Extensions;
+using Library.Types;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
+using Library.Managers;
+
+namespace TeamPanel.Pages
+{
+    public class BetausersDeleteModel : PageModel
+    {
+        private MySqlConnection MySQL;
+        public Account LoginUser;
+        public BetausersDeleteModel(MySqlConnection mySQL)
+        {
+            MySQL = mySQL;
+        }
+
+        public IActionResult OnGet(int id, string redirect)
+        {
+            // Authorization
+            AuthorizationResult authorizationResult;
+            if (!Authorization.CheckAuthorization(HttpContext, MySQL, HttpContext.Response, out authorizationResult)) { return StatusCode(authorizationResult.StatusCode); }
+            LoginUser = authorizationResult.Account;
+
+            if (!String.IsNullOrEmpty(Convert.ToString(id)))
+            {
+                BetaUser DeleteBetauser = MySQL.Get<BetaUser>(id);
+                MySQL.Delete(DeleteBetauser);
+            }
+            if (String.IsNullOrEmpty(redirect))
+            {
+                return Redirect("/Betausers");
+            }
+            else
+            {
+                return Redirect(redirect);
+            }
+        }
+    }
+}
