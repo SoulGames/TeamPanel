59db4786cd9f3ef66b4ddaa022bc9a3982c06816
No Name Pro <noname.pro.nn@gmail.com>
2020-07-23 16:29:16 +0200

    🚧 General preparing for Tasks

---
+++ Library/Types/Task.cs
@@ -0,0 +1,17 @@
+﻿using Dapper.Contrib.Extensions;
+using System;
+
+namespace Library.Types
+{
+    [Table("tasks")]
+    public class Task
+    {
+        [ExplicitKey]
+        public int ID { get; set; }
+        public string TITLE { get; set; }
+        public string PROJECT { get; set; }
+        public int? ASSIGNED { get; set; }
+        public int? CREATOR { get; set; }
+        public DateTime CREATED_AT { get; set; }
+    }
+}
